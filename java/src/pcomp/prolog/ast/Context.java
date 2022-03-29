00b65d75957f2c3a24e98f4262bb7000328bd21d
~revers <~revers@DESKTOP-TEAFJ8V>
2022-03-29 21:41:11 +0200

    ajout classe contexte

---
+++ java/src/pcomp/prolog/ast/Context.java
@@ -0,0 +1,19 @@
+package pcomp.prolog.ast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Context {
+	public final List<DeclGoal> goals;
+	public final List<DeclAssertion> rules;
+	public final Environnement env;
+	
+	public Context(List<DeclGoal> goals, List<DeclAssertion> rules, Environnement env) {
+		this.goals=new ArrayList<>();
+		this.rules=new ArrayList<>();
+		this.env=new Environnement();
+		this.goals.addAll(goals);
+		this.rules.addAll(rules);
+		this.env.putAll(env);
+	}
+}
